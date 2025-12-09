b716d74d3a85c2603ece22fc1f992b1ab31d1ab9
nazzato-hub <n.azzato@studenti.unisa.it>
2025-12-09 12:49:54 +0100

    modifica Interfaccia

--- src/main/java/it/unisa/diem/gruppo01/interfacce/Menu_BibliotecaController.java
+++ src/main/java/it/unisa/diem/gruppo01/interfacce/Menu_BibliotecaController.java
@@ -6,12 +6,20 @@ package it.unisa.diem.gruppo01.interfacce;
  * and open the template in the editor.
  */
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -39,18 +47,84 @@ public class Menu_BibliotecaController implements Initializable {
 
     @FXML
     private void openLibriInterface(ActionEvent event) {
+        
+        try{
+            
+        Parent libriParent = FXMLLoader.load(getClass().getResource("/it/unisa/diem/gruppo01/interfacce/GestioneLibri_view.fxml"));
+        Scene libriScene = new Scene(libriParent);
+        
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(libriScene);
+        window.setTitle("Menù Principale - Gestione Libri");
+        window.show();
+        
+    }catch (IOException ex){
+        System.out.println("ERRORE critico nel caricamento di Gestione Libri");
+        ex.printStackTrace();
+      }
     }
+    
+    
 
     @FXML
-    private void openStudentInterface(ActionEvent event) {
-    }
+    private void openStudentInterface(ActionEvent event){
+        
+        
+            
+            try {
+                Parent studentiParent = FXMLLoader.load(getClass().getResource("/it/unisa/diem/gruppo01/interfacce/GestioneStudente_view.fxml"));
+                Scene studentiScene = new Scene(studentiParent);
+                
+                Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+                window.setScene(studentiScene);
+                window.setTitle("Menù Principale - Gestione Studenti");
+                window.show();
+                
+                
+            } catch (IOException ex) {
+               System.out.println("ERRORE critico nel caricamento di Gestione Studenti");
+               ex.printStackTrace();
+            }
+        }
+    
 
     @FXML
     private void openPrestitiInterface(ActionEvent event) {
+        
+        
+                try {
+                Parent prestitiParent = FXMLLoader.load(getClass().getResource("/it/unisa/diem/gruppo01/interfacce/Interfaccia_nuovoPrestito.fxml"));
+                Scene prestitiScene = new Scene(prestitiParent);
+                
+                Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+                window.setScene(prestitiScene);
+                window.setTitle("Menù Principale - Gestione Prestiti");
+                window.show();
+                
+                
+            } catch (IOException ex) {
+               System.out.println("ERRORE critico nel caricamento di Gestione Prestiti");
+               ex.printStackTrace();
+            }
     }
 
     @FXML
     private void openExitInterface(ActionEvent event) {
+        
+                try {
+                Parent loginParent = FXMLLoader.load(getClass().getResource("/it/unisa/diem/gruppo01/interfacce/Interfaccia1View.fxml"));
+                Scene loginScene = new Scene(loginParent);
+                
+                Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+                window.setScene(loginScene);
+                window.setTitle("Menù Principale - Login");
+                window.show();
+                
+                
+            } catch (IOException ex) {
+               System.out.println("ERRORE critico nel ritorno al Login");
+               ex.printStackTrace();
+            }
     }
     
 }
